767279e7648784adbd4a463e4064443848e18684
MiniSB <51980101+MiniSB@users.noreply.github.com>
2021-06-27 17:34:04 +1000

    Can change address
    Bug fixes to character length display

--- main.c
+++ main.c
@@ -11,55 +11,80 @@
 
 #define WIDTH 40
 #define HEIGHT 40
-#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+#define gotoxy(x, y) printf("\033[%d;%dH", (y), (x))
 
-int PORT= 8080;
-char addr[15];
+int PORT = 8080;
+char ADDRESS[15];
 char GC[HEIGHT][WIDTH];
 
 /*__________________________HELPER FUNCTIONS__________________________*/
 //Move Cursor
-void cursormove(int x, int y){
+void cursormove(int x, int y)
+{
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD pos = {x, y};
-    SetConsoleCursorPosition(hConsole, pos);
+	COORD pos = {x, y};
+	SetConsoleCursorPosition(hConsole, pos);
 }
 
 //Change Cursor Visibility
 void cursor(bool visibility)
 {
-	CONSOLE_CURSOR_INFO cursor;    
-	cursor.bVisible = visibility;    
-	cursor.dwSize = sizeof(cursor);    
-	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);    
+	CONSOLE_CURSOR_INFO cursor;
+	cursor.bVisible = visibility;
+	cursor.dwSize = sizeof(cursor);
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorInfo(handle, &cursor);
 }
 
 //clear console screen
-void clrscr(){ system("@cls||clear"); }
+void clrscr() { system("@cls||clear"); }
 
 //Minimum of 2 values
-int minimum(int i, int j){
-	if(i < j){return i;}else{return j;}
+int minimum(int i, int j)
+{
+	if (i < j)
+	{
+		return i;
+	}
+	else
+	{
+		return j;
+	}
 }
 
 //Range of 2 values
-int range(int i, int j){
-	if(i < j){return abs(j-i);}else{return abs(i-j);}
+int range(int i, int j)
+{
+	if (i < j)
+	{
+		return abs(j - i);
+	}
+	else
+	{
+		return abs(i - j);
+	}
 }
 
 //Length of pointer
-int pointerlen(char c[]){
-	int i=1; while(c[i]!='\0'){i++;};return i;
+int pointerlen(char c[])
+{
+	int i = 1;
+	while (c[i] != '\0')
+	{
+		i++;
+	};
+	return i;
 }
 
 //return position for text to be centred
-int centertext(char c[]){
-	return (WIDTH-pointerlen(c))/2;
+int centertext(char c[])
+{
+	return (WIDTH - pointerlen(c)) / 2;
 }
 /*_________________________GRAPHICS LIBRARY____________________________*/
 
-void clearcanvas(){
+void clearcanvas()
+{
 	// for(int i=0; i<HEIGHT;i++){
 	// 	for(int j=0;j<WIDTH;j++){
 	// 		GC[i][j] = ' ';
@@ -68,108 +93,144 @@ void clearcanvas(){
 	memset(GC, ' ', sizeof(GC[0][0]) * WIDTH * HEIGHT);
 }
 
-void drawscreen(){
+void drawscreen()
+{
 	//Moves windows handle to console beginning
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD pos = {0, 0};
-    SetConsoleCursorPosition(hConsole, pos);
-	for(int i=0; i<HEIGHT;i++){
+	COORD pos = {0, 0};
+	SetConsoleCursorPosition(hConsole, pos);
+	for (int i = 0; i < HEIGHT; i++)
+	{
 		putchar('\r');
-		for(int j=0;j<WIDTH;j++){
+		for (int j = 0; j < WIDTH; j++)
+		{
 			putchar(GC[i][j]);
 		}
 		putchar('\n');
 	}
 }
 
-void drawline(int xi, int yi, int xf, int yf, char c){
+void drawline(int xi, int yi, int xf, int yf, char c)
+{
 	//Check if line is constant or sloped
-	if(xi == xf || yi == yf){
-		if(xi == xf){
+	if (xi == xf || yi == yf)
+	{
+		if (xi == xf)
+		{
 			//Draw horizontal line
-			for (int i = 0; i < range(yi,yf) || range(yi,yf) == i; i++){ GC[minimum(yi,yf)+i][xi] = c;}	
-		}else{
-			for (int i = 0; i < range(xi,xf) || range(xi,xf) == i; i++){ GC[yi][minimum(xi,xf)+i] = c;}
+			for (int i = 0; i < range(yi, yf) || range(yi, yf) == i; i++)
+			{
+				GC[minimum(yi, yf) + i][xi] = c;
+			}
 		}
-	}else{
-		float grad = (yf-yi)/(xf-xi);
-		float con = yi-(grad*xi);
+		else
+		{
+			for (int i = 0; i < range(xi, xf) || range(xi, xf) == i; i++)
+			{
+				GC[yi][minimum(xi, xf) + i] = c;
+			}
+		}
+	}
+	else
+	{
+		float grad = (yf - yi) / (xf - xi);
+		float con = yi - (grad * xi);
 
-		if(range(xi,xf) > range(yi,yf)){
-			for(int i=minimum(xi,xf); i < (minimum(xi,xf)+range(xi,xf));i++){ GC[(int)round((grad*i)+con)][i] = c;}
-		}else{
-			for(int i=minimum(yi,yf); i < (minimum(yi,yf)+range(yi,yf));i++){ GC[i][(int)round((i-con)/grad)] = c;}
+		if (range(xi, xf) > range(yi, yf))
+		{
+			for (int i = minimum(xi, xf); i < (minimum(xi, xf) + range(xi, xf)); i++)
+			{
+				GC[(int)round((grad * i) + con)][i] = c;
+			}
+		}
+		else
+		{
+			for (int i = minimum(yi, yf); i < (minimum(yi, yf) + range(yi, yf)); i++)
+			{
+				GC[i][(int)round((i - con) / grad)] = c;
+			}
 		}
 	}
 }
 
-void drawchar(int x, int y, char c){
-	if(x>=0 && x<=HEIGHT && y>=0 && y<=HEIGHT){
+void drawchar(int x, int y, char c)
+{
+	if (x >= 0 && x <= HEIGHT && y >= 0 && y <= HEIGHT)
+	{
 		GC[y][x] = c;
 	}
 }
 
 //Initialises game state
-void writescreen(int posx, int posy, char c[]){
+void writescreen(int posx, int posy, char c[])
+{
 	int len = pointerlen(c);
 	// printf("%d", len);
-	if(posy <= HEIGHT && posy > 0){
+	if (posy <= HEIGHT && posy > 0)
+	{
 		int len = len;
-		if((len + posx) > WIDTH){
+		if ((len + posx) > WIDTH)
+		{
 			len = WIDTH - posx;
 		}
-		for(int i=0; i< len && c[i]!= '\0';i++){
-			GC[posy][posx+i] = c[i];
+		for (int i = 0; i < len && c[i] != '\0'; i++)
+		{
+			GC[posy][posx + i] = c[i];
 		}
 	}
 }
 
-void drawborder(){
-	drawline(0, 0, 0, HEIGHT-1, '|');
-	drawline(WIDTH-1, 0, WIDTH-1, HEIGHT-1, '|');
-	drawline(0, 0, WIDTH-1, 0, '-');
-	drawline(0, HEIGHT-1, WIDTH-1, HEIGHT-1, '-');
+void drawborder()
+{
+	drawline(0, 0, 0, HEIGHT - 1, '|');
+	drawline(WIDTH - 1, 0, WIDTH - 1, HEIGHT - 1, '|');
+	drawline(0, 0, WIDTH - 1, 0, '-');
+	drawline(0, HEIGHT - 1, WIDTH - 1, HEIGHT - 1, '-');
 }
 /*____________________________________________________________________*/
 //change port
-void portchange(){
+void portchange()
+{
 	clearcanvas();
 	drawborder();
-	writescreen(centertext("Changing Port"),10, "Changing Port");
-	writescreen(centertext("New Port: "),20, "New Port: ");
+	writescreen(centertext("Changing Port"), 10, "Changing Port");
+	writescreen(centertext("New Port: "), 20, "New Port: ");
 
 	drawscreen();
 	char prt[4];
 
-	cursormove(centertext("New Port: ")+pointerlen("New Port: ")+1, 20);
+	cursormove(centertext("New Port: ") + pointerlen("New Port: ") + 1, 20);
 	cursor(TRUE);
-	scanf("%s", prt);
+	scanf("%5s", prt);
 	sscanf(prt, "%d", &PORT);
 
 	clearcanvas();
 	drawborder();
-	writescreen(centertext("Changing Port"),10, "Changing Port");
+	writescreen(centertext("Changing Port"), 10, "Changing Port");
 	writescreen(centertext("Port Changed"), 20, "Port Changed");
 	writescreen(centertext("Press any key to continue"), 35, "Press any key to continue");
 	cursor(FALSE);
 	drawscreen();
-	getch();	
+	getch();
 }
 
-void addresschange(){
+void addresschange()
+{
 	clearcanvas();
 	drawborder();
-	writescreen(centertext("Changing Address"),10, "Changing Address");
-	writescreen(centertext("New Address: "),20, "New Address: ");
+	writescreen(centertext("Changing Address"), 10, "Changing Address");
+	writescreen(centertext("New Address:") / 2, 20, "New Address:");
 
 	drawscreen();
-	cursormove(centertext("New Address: ")+pointerlen("New Address: ")+1, 20);
+	cursormove(centertext("New Address: ") / 2 + pointerlen("New Address: ") + 1, 20);
 	cursor(TRUE);
-	scanf("%s", addr);
+	char address[10];
+	scanf("%14s", address);
+	sscanf(address, "%14s", ADDRESS);
 
 	clearcanvas();
 	drawborder();
-	writescreen(centertext("Changing Address"),10, "Changing Address");
+	writescreen(centertext("Changing Address"), 10, "Changing Address");
 	writescreen(centertext("Address Changed"), 20, "Address Changed");
 	writescreen(centertext("Press any key to continue"), 35, "Press any key to continue");
 	cursor(FALSE);
@@ -177,14 +238,16 @@ void addresschange(){
 	getch();
 }
 
-//Client 
-void clientmenu(){
+//Client
+void clientmenu()
+{
 	//client menu loop
 
 	int option = 0;
 	bool loop = false;
 
-	while(!loop){
+	while (!loop)
+	{
 		clearcanvas();
 		drawborder();
 
@@ -195,75 +258,102 @@ void clientmenu(){
 		sprintf(prt, "%d", PORT);
 
 		//concate port message
-		char cport[] = "Port: ";
+		char cport[13] = "Port: ";
 		strcat(cport, prt);
 		writescreen(centertext(cport), 10, cport);
 
-		//concate address message
-		char caddr[] = "Address: ";
-		strcat(caddr, addr);
+		// concate address message
+		char caddr[26] = "Address: ";
+		strcat(caddr, ADDRESS);
 		writescreen(centertext(caddr), 11, caddr);
 
-		if(option == 0){
+		if (option == 0)
+		{
 			writescreen(centertext("1. Change Port"), 20, " -> 1. Change Port");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("1. Change Port"), 20, "1. Change Port");
 		}
 
-		if(option == 1){
+		if (option == 1)
+		{
 			writescreen(centertext("2. Change Address"), 25, " -> 2. Change Address");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("2. Change Address"), 25, "2. Change Address");
 		}
 
-		if(option == 2){
+		if (option == 2)
+		{
 			writescreen(centertext("3. Join Game"), 30, " -> 3. Join Game");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("3. Join Game"), 30, "3. Join Game");
 		}
 
-		if(option == 3){
+		if (option == 3)
+		{
 			writescreen(centertext("4. Back"), 35, " -> 4. Back");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("4. Back"), 35, "4. Back");
 		}
 		drawscreen();
 
 		char ip = getch();
 
-		if(ip =='s' || ip =='S'){
+		if (ip == 's' || ip == 'S')
+		{
 			option++;
-		}else if(ip == 'w' || ip =='W'){
+		}
+		else if (ip == 'w' || ip == 'W')
+		{
 			option--;
 		}
 
-		if(option >3){
+		if (option > 3)
+		{
 			option = 0;
-		}else if(option < 0){
+		}
+		else if (option < 0)
+		{
 			option = 3;
 		}
 
-		if(ip =='f' || ip =='F'){
-			if(option == 0){
+		if (ip == 'f' || ip == 'F')
+		{
+			if (option == 0)
+			{
 				portchange();
-			}else if(option == 1){
+			}
+			else if (option == 1)
+			{
 				addresschange();
-			}else if(option == 2){
+			}
+			else if (option == 2)
+			{
 				//Join game
-			}else if(option == 3){
+			}
+			else if (option == 3)
+			{
 				loop = true;
 			}
 		};
 	}
 }
 
-
 //Server
-void servermenu(){
-		
+void servermenu()
+{
+
 	int option = 0;
 	bool loop = false;
-	while (!loop){
+	while (!loop)
+	{
 
 		//server menu loop
 		clearcanvas();
@@ -280,92 +370,124 @@ void servermenu(){
 		writescreen(centertext("Hosting Game"), 5, "Hosting Game");
 		writescreen(centertext(cport), 10, cport);
 
-		if(option == 0){
+		if (option == 0)
+		{
 			writescreen(centertext("1. Change Port"), 20, " -> 1. Change Port");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("1. Change Port"), 20, "1. Change Port");
 		}
 
-		if(option == 1){
+		if (option == 1)
+		{
 			writescreen(centertext("2. Create Game"), 25, " -> 2. Create Game");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("2. Create Game"), 25, "2. Create Game");
 		}
 
-		if(option == 2){
+		if (option == 2)
+		{
 			writescreen(centertext("3. Back"), 30, " -> 3. Back");
-		}else{
+		}
+		else
+		{
 			writescreen(centertext("3. Back"), 30, "3. Back");
 		}
 		drawscreen();
 
 		char ip = getch();
 
-		if(ip =='s' || ip =='S'){
+		if (ip == 's' || ip == 'S')
+		{
 			option++;
-		}else if(ip == 'w' || ip =='W'){
+		}
+		else if (ip == 'w' || ip == 'W')
+		{
 			option--;
 		}
 
-		if(option >2){
+		if (option > 2)
+		{
 			option = 0;
-		}else if(option < 0){
+		}
+		else if (option < 0)
+		{
 			option = 2;
 		}
 
-		if(ip =='f' || ip =='F'){
-			if(option == 0){
+		if (ip == 'f' || ip == 'F')
+		{
+			if (option == 0)
+			{
 				portchange();
-			}else if(option == 1){
+			}
+			else if (option == 1)
+			{
 				//Create Game
-			}else if(option == 2){
+			}
+			else if (option == 2)
+			{
 				loop = true;
 			}
 		};
-
 	}
-	
 }
 
-
 //Main Menu
-void mainmenu(int option){
+void mainmenu(int option)
+{
 	//Set initial Game Screen
 	clearcanvas();
 	drawborder();
 	writescreen(centertext("Pong, Multiplayer Game"), 10, "Pong, Multiplayer Game");
 
-	if(option == 0){
+	if (option == 0)
+	{
 		writescreen(centertext("1. Create Game"), 20, " -> 1. Create Game");
-	}else{
+	}
+	else
+	{
 		writescreen(centertext("1. Create Game"), 20, "1. Create Game");
 	}
 
-	if(option == 1){
+	if (option == 1)
+	{
 		writescreen(centertext("2. Join Game"), 25, " -> 2. Join Game");
-	}else{
+	}
+	else
+	{
 		writescreen(centertext("2. Join Game"), 25, "2. Join Game");
 	}
 
-	if(option == 2){
+	if (option == 2)
+	{
 		writescreen(centertext("3. Exit"), 30, " -> 3. Exit");
-	}else{
+	}
+	else
+	{
 		writescreen(centertext("3. Exit"), 30, "3. Exit");
 	}
 	// writescreen((WIDTH-pointerlen("Pong, Multipleayer Game"))/2);
 }
 
-void init(){
+void init()
+{
 	//Set game board to empty
 	clrscr();
-	for(int i=0; i<WIDTH;i++){
-		for(int j=0;j<HEIGHT;j++){
+	for (int i = 0; i < WIDTH; i++)
+	{
+		for (int j = 0; j < HEIGHT; j++)
+		{
 			GC[i][j] = ' ';
 		}
 	}
 }
 
-int main(int argc , char *argv[]){
+int main(int argc, char *argv[])
+{
 	//Change Window Title
 	SetConsoleTitle("Pong but better");
 	cursor(FALSE);
@@ -376,29 +498,42 @@ int main(int argc , char *argv[]){
 	int option = 0;
 
 	bool valid = false;
-	while(!valid){
+	while (!valid)
+	{
 		mainmenu(option);
 		drawscreen();
 		char ip = getch();
-		
-		if(ip =='s' || ip =='S'){
+
+		if (ip == 's' || ip == 'S')
+		{
 			option++;
-		}else if(ip == 'w' || ip =='W'){
+		}
+		else if (ip == 'w' || ip == 'W')
+		{
 			option--;
 		}
 
-		if(option >2){
+		if (option > 2)
+		{
 			option = 0;
-		}else if(option < 0){
+		}
+		else if (option < 0)
+		{
 			option = 2;
 		}
 
-		if(ip =='f' || ip =='F'){
-			if(option == 0){
+		if (ip == 'f' || ip == 'F')
+		{
+			if (option == 0)
+			{
 				servermenu();
-			}else if(option == 1){
+			}
+			else if (option == 1)
+			{
 				clientmenu();
-			}else if(option == 2){
+			}
+			else if (option == 2)
+			{
 				valid = true;
 			}
 		};
